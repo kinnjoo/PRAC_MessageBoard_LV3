c8636e3efa5b43e28527f9af91d4603fc7ba3d99
Kim Joohee <syap1170@gmail.com>
2023-07-10 10:51:26 +0900

    feat: postsController

---
+++ controllers/posts.controller.js
@@ -0,0 +1,27 @@
+// Post의 컨트롤러 역할을 하는 클래스
+class PostsController {
+  postService = new PostService(); // Post 서비스를 클래스를 컨트롤러 클래스의 멤버 변수로 할당
+
+  getPosts = async (req, res, next) => {
+    // 서비스 계층에 구현된 findAllPost 로직 실행
+    const posts = await this.postService.findAllPost();
+
+    res.status(200).json({ posts });
+  };
+
+  createPost = async (req, res, next) => {
+    const { nickname, password, title, content } = req.body;
+
+    // 서비스 계층에 구현된 createPost 로직을 실행
+    const createPostData = await this.postService.createPost(
+      nickname,
+      password,
+      title,
+      content
+    );
+
+    res.status(201).json({ createPostData });
+  };
+}
+
+module.exports = PostsController;
